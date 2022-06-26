d41f71dff996c150c45cef667df2c69c8e982472
Apache553 <Apache553@outlook.com>
2022-06-26 16:42:50 +0800

    multi-threaded rpc server

--- SubtitleFontAutoLoaderDaemon/RpcServer.cpp
+++ SubtitleFontAutoLoaderDaemon/RpcServer.cpp
@@ -3,6 +3,7 @@
 #include "RpcServer.h"
 
 #define WIN32_LEAN_AND_MEAN
+#include <queue>
 #include <Windows.h>
 #include <wil/resource.h>
 
@@ -12,32 +13,88 @@ private:
 	IDaemon* m_daemon;
 	IRpcRequestHandler* m_handler;
 
-	std::thread m_worker;
+	std::thread m_listener;
 	std::atomic<size_t> m_checkPoint;
 	wil::unique_event m_exitEvent;
 
 
+	void ProcessNewRequest(wil::unique_handle& pipe) noexcept
+	{
+		OVERLAPPED overlapped{};
+		ResetOverlapped(&overlapped);
+		try
+		{
+			AcceptRequest(pipe, &overlapped);
+		}
+		catch (...)
+		{
+			// ignore exceptions
+		}
+		FlushFileBuffers(pipe.get());
+		DisconnectNamedPipe(pipe.get());
+	}
+
+	void DoWork(std::condition_variable& cv, std::mutex& lock, std::queue<wil::unique_handle>& pending, bool& stop)
+	{
+		std::unique_lock lg(lock);
+		while (!stop)
+		{
+			cv.wait(lg, [&]() { return !pending.empty() || stop; });
+			while (!pending.empty())
+			{
+				auto conn = std::move(pending.front());
+				pending.pop();
+				lg.unlock();
+
+				ProcessNewRequest(conn);
+
+				lg.lock();
+			}
+		}
+	}
+
+
 public:
+	static constexpr size_t WORKER_COUNT = 4;
+
 	Implementation(IDaemon* daemon, IRpcRequestHandler* handler)
 		: m_daemon(daemon), m_handler(handler), m_checkPoint(0)
 	{
 		m_exitEvent.create(wil::EventOptions::ManualReset);
-		m_worker = std::thread([&]()
+		m_listener = std::thread([&]()
 		{
 			try
 			{
-				OVERLAPPED overlapped;
+				std::condition_variable workerCV;
+				std::mutex workerMutex;
+				std::queue<wil::unique_handle> workerQueue;
+				bool stop = false;
+				std::vector<std::thread> workers;
+
+
+				workers.reserve(WORKER_COUNT);
+				for (size_t i = 0; i < WORKER_COUNT; ++i)
+				{
+					workers.emplace_back([&]()
+					{
+						DoWork(workerCV, workerMutex, workerQueue, stop);
+					});
+				}
+
 				wil::unique_event connectedEvent;
 				connectedEvent.create(wil::EventOptions::ManualReset);
 				HANDLE waitList[2] = {
 					connectedEvent.get(),
 					m_exitEvent.get()
 				};
+
+				OVERLAPPED overlapped;
 				overlapped.hEvent = connectedEvent.get();
-				auto pipe = CreateNewNamedPipe();
-				++m_checkPoint;
+
 				while (!m_exitEvent.is_signaled())
 				{
+					auto pipe = CreateNewNamedPipe();
+					++m_checkPoint;
 					ResetOverlapped(&overlapped);
 					ConnectNamedPipe(pipe.get(), &overlapped);
 					THROW_LAST_ERROR_IF(GetLastError() != ERROR_IO_PENDING && GetLastError() != ERROR_PIPE_CONNECTED);
@@ -51,20 +108,21 @@ public:
 					if (waitResult == WAIT_OBJECT_0)
 					{
 						// connected
-						try
-						{
-							AcceptRequest(pipe, &overlapped);
-						}
-						catch (...)
-						{
-							// ignore exceptions
-						}
-						FlushFileBuffers(pipe.get());
-						DisconnectNamedPipe(pipe.get());
+						std::lock_guard lg(workerMutex);
+						workerQueue.push(std::move(pipe));
+						workerCV.notify_one();
 					}
 					else if (waitResult == WAIT_OBJECT_0 + 1)
 					{
 						// exit
+						std::unique_lock lg(workerMutex);
+						stop = true;
+						workerCV.notify_all();
+						lg.unlock();
+						for (auto& worker : workers)
+						{
+							worker.join();
+						}
 						break;
 					}
 					else if (waitResult == WAIT_FAILED)
@@ -90,8 +148,8 @@ public:
 	~Implementation()
 	{
 		m_exitEvent.SetEvent();
-		if (m_worker.joinable())
-			m_worker.join();
+		if (m_listener.joinable())
+			m_listener.join();
 	}
 
 private:
@@ -164,8 +222,11 @@ private:
 	{
 		HANDLE hEvent = overlapped->hEvent;
 		memset(overlapped, 0, sizeof(OVERLAPPED));
-		overlapped->hEvent = hEvent;
-		ResetEvent(hEvent);
+		if (hEvent != nullptr)
+		{
+			overlapped->hEvent = hEvent;
+			ResetEvent(hEvent);
+		}
 	}
 };
 
