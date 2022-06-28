e43197cb125c4cb654711d1e6e567ba3310cbc27
Apache553 <Apache553@outlook.com>
2022-06-28 22:59:08 +0800

    update workflow

--- copyFiles.ps1
+++ copyFiles.ps1
@@ -1,5 +1,10 @@
 $scriptpath = $MyInvocation.MyCommand.Path
 $scriptdir = Split-Path $scriptpath
+$configuration = $env:BUILD_CONFIGURATION
+if($configuration -eq $null)
+{
+	$configuration = "Release"
+}
 
 Set-Location $scriptdir
 New-Item ReleaseBuild -ItemType Directory
@@ -11,7 +16,7 @@ Copy-Item ../unregisterETW.ps1 .
 Copy-Item ../enableAutoStart.ps1 .
 Copy-Item ../disableAutoStart.ps1 .
 Copy-Item ../SubtitleFontHelper.example.xml .
-Copy-Item ../Build/Win32/Release/FontLoadInterceptor32.dll .
-Copy-Item ../Build/x64/Release/FontLoadInterceptor64.dll .
-Copy-Item ../Build/x64/Release/FontDatabaseBuilder.exe .
-Copy-Item ../Build/x64/Release/SubtitleFontAutoLoaderDaemon.exe .
+Copy-Item ../Build/Win32/$configuration/FontLoadInterceptor32.dll .
+Copy-Item ../Build/x64/$configuration/FontLoadInterceptor64.dll .
+Copy-Item ../Build/x64/$configuration/FontDatabaseBuilder.exe .
+Copy-Item ../Build/x64/$configuration/SubtitleFontAutoLoaderDaemon.exe .
